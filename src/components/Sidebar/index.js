ef052e9c5c7f671e33ef2e1e05162906ecc63ca1
Patrick O'Connor <pattyo20@optonline.net>
2023-01-17 19:39:48 -0500

    initialized project and set up links and logos

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,38 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import plogo from '../../assets/images/plogo.png'
+import patrickoconnor from '../../assets/images/patrickoconnor.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={plogo} alt="logo" />
+      <img className="sub-logo" src={patrickoconnor} alt="patrick" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+  </div>
+)
+
+export default Sidebar
