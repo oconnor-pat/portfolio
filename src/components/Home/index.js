f16212d5dd83ae7ac9ddc8e332cc934625e7767b
Patrick O'Connor <pattyo20@optonline.netm>
2023-03-27 17:01:19 -0400

    updated job title and skills on home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -27,45 +27,46 @@ const Home = () => {
   ]
 
   useEffect(() => {
-    return() => setTimeout(() => {
+    return () =>
+      setTimeout(() => {
         setLetterClass('text-animate-hover')
-    }, 4000)
+      }, 4000)
   }, [])
 
   return (
     <>
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={plogo} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>
-          Frontend Developer | HTML | CSS | JavaScript | React | Redux |
-          Express.js | Node.js |{' '}
-        </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={plogo} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>
+            Software Engineer | HTML | CSS | JavaScript | TypeScript | React |
+            Redux | Node.js | Java | Swift | Jest{' '}
+          </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
-    <Loader type='ball-pulse' />
+      <Loader type="ball-pulse" />
     </>
   )
 }
